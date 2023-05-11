5242f50e143048fd679c2f851ce0e85d1b69333d
Your <dell0@DELL>
2023-05-11 17:11:04 +0200

    add image

--- run2.bat
+++
@@ -1,3 +0,0 @@
-
-@echo off
-cmd /k "cd /d C:\Users\dell0\PycharmProjects\MakeImageSquare\venv\Scripts & activate & cd /d C:\Users\dell0\PycharmProjects\MakeImageSquare & python ImageEditor.py -r %*"
